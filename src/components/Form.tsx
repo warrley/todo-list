09227be7c3140b5d2da507deed42cf70052d329c
warley farias <157419864+warrley@users.noreply.github.com>
2025-02-06 20:15:20 -0300

    add to mobile

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -19,10 +19,10 @@ export const Form = () => {
     };
 
     return (
-        <div className="gap-8 w-2/4 ring-2 ring-sky-500/90 bg-blue-900 rounded-lg flex items-center justify-center flex-col p-8 shadow-md shadow-sky-500/80">
-            <h1 className="text-5xl font-semibold">Todo List</h1>
+        <div className="gap-4 lg:gap-8 w-5/6 lg:w-2/4 ring-2 ring-sky-500/90 bg-blue-900 rounded-lg flex items-center justify-center flex-col p-4 lg:p-8 shadow-md shadow-sky-500/80">
+            <h1 className="lg:text-5xl text-3xl font-semibold">Todo List</h1>
 
-            <div className="w-full flex rounded-xl text-2xl">
+            <div className="flex  rounded-xl text-md lg:text-2xl">
                 <input
                     value={title}
                     onKeyDown={(e) => {
@@ -31,7 +31,7 @@ export const Form = () => {
                     onChange={(e) => setTitle(e.target.value)}
                     placeholder="What's the task today?"
                     type="text"
-                    className="outline-none text-blue-900 p-4 flex-1 rounded-md rounded-r-none"
+                    className="outline-none text-blue-900 px-3 lg:p-4 flex-1 rounded-md rounded-r-none"
                 />
                 <button onClick={handleAdd} className="py-2 px-3 bg-sky-500 rounded-r-md">
                     Add Task
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -8,9 +8,9 @@ export const Item = () => {
     const ItemsCtx = useContext(ItemsContext)
 
     return(
-        <div className="flex flex-col gap-4 w-full text-2xl">
+        <div className="flex flex-col gap-4 w-full text-lg lg:text-2xl">
             {ItemsCtx?.items.map(item => (
-                <div className="bg-sky-500 py-3 px-4 rounded-md flex justify-between"key={item.id}>
+                <div className="bg-sky-500 py-2 px-3 lg:py-3 lg:px-4 rounded-md flex justify-between"key={item.id}>
                     {item.title}
                     <div className="flex gap-4">
                         <p className="cursor-pointer"><FontAwesomeIcon icon={faCheck}/></p>
