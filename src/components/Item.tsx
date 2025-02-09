2e0fec2f780b8e27ee83cb3834e1e15bc8a5f08c
warley farias <157419864+warrley@users.noreply.github.com>
2025-02-09 08:59:42 -0300

    fix vercel checked

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,8 +5,6 @@ import { ItemsContext } from "@/context/ItemContext";
 
 export const Item = () => {
   const ItemsCtx = useContext(ItemsContext);
-  const [hoverDelete, setHoverDelete] = useState<number | null>(null);
-  const [hoverCheck, setHoverCheck] = useState<number | null>(null);
 
   if (!ItemsCtx || !ItemsCtx?.items) return; //verifica para garantir que ItemsCtx não seja undefined
 
@@ -27,39 +25,19 @@ export const Item = () => {
     );
   };
 
-  const hoverTrueDelete = (id: number) => {
-    setHoverDelete(id);
-  };
-
-  const hoverFalseDelete = () => {
-    setHoverDelete(null);
-  };
-
-  const hoverTrueCheck = (id: number) => {
-    setHoverCheck(id);
-  };
-
-  const hoverFalseCheck = () => {
-    setHoverCheck(null);
-  };
-
   return (
     <div className="flex flex-col gap-4 w-full text-lg lg:text-2xl">
       {ItemsCtx?.items.map((item) => (
         <div
-          className={`${
-            item.checked ? "bg-green-500" : "bg-sky-500"
-          } transition-all duration-200 py-2 px-3 lg:py-3 lg:px-4 rounded-md flex justify-between`}
+          className={`
+            ${
+              item.checked ? "bg-green-500" : "bg-sky-500"
+            }  transition-all duration-200 py-2 px-3 lg:py-3 lg:px-4 rounded-md flex justify-between`}
           key={item.id}
         >
           <p className={`${item.checked && "line-through"}`}>{item.title}</p>
           <div className="flex gap-4">
-            <p
-              className="cursor-pointer"
-              onMouseOver={() => hoverTrueCheck(item.id)}
-              onMouseLeave={() => hoverFalseCheck()}
-              onClick={() => handleCheck(item.id)}
-            >
+            <p className="cursor-pointer" onClick={() => handleCheck(item.id)}>
               <FontAwesomeIcon icon={faCheck} />
             </p>
             <p
